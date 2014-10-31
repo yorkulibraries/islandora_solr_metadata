8b1da8349f54277eead1508bafdc4959804ab76b
nruest <ruestn@gmail.com>
2014-10-31 18:09:50 -0400

    YUDL doesn't like the collapsible element here. I VANQUISH THEE

--- theme/islandora-solr-metadata-display.tpl.php
+++ theme/islandora-solr-metadata-display.tpl.php
@@ -20,7 +20,7 @@
 ?>
 <?php if ($found):?>
    <?php if (!(empty($solr_fields) && variable_get('islandora_solr_metadata_omit_empty_values', FALSE))):?>
-<fieldset <?php $print ? print('class="islandora islandora-metadata"') : print('class="islandora islandora-metadata collapsible collapsed"');?>>
+<fieldset <?php $print ? print('class="islandora islandora-metadata"') : print('class="islandora islandora-metadata"');?>>
   <legend><span class="fieldset-legend"><?php print t('Details'); ?></span></legend>
   <div class="fieldset-wrapper">
     <dl xmlns:dcterms="http://purl.org/dc/terms/" class="islandora-inline-metadata islandora-metadata-fields">
@@ -39,7 +39,7 @@
 </fieldset>
   <?php endif; ?>
 <?php else: ?>
-  <fieldset <?php $print ? print('class="islandora islandora-metadata"') : print('class="islandora islandora-metadata collapsible collapsed"');?>>
+  <fieldset <?php $print ? print('class="islandora islandora-metadata"') : print('class="islandora islandora-metadata"');?>>
     <legend><span class="fieldset-legend"><?php print t('Details'); ?></span></legend>
     <?php //XXX: Hack in markup for message. ?>
     <div class="messages--warning messages warning">
